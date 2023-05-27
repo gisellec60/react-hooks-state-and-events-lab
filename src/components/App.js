5256a40491b1de9c4f8005dec29d0e0d965b66dc
Giselle <gisellec60@gmail.com>
2023-05-27 12:08:25 -0400

    test will not pass

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,40 @@
-import React from "react";
-import ShoppingList from "./ShoppingList";
+import React, {useState} from "react";
+import ShoppingList from "./ShoppingList"; 
 import itemData from "../data/items";
 
 function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+   //const appClass = islight ? "App dark" : "App light"
+  
+  const [isLight, setIsLight] = useState(false) 
 
+  function chgMode () {
+    setIsLight(isLight => !isLight) 
+  } 
+
+  const appClass = isLight ? "App dark" : "App light"
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={chgMode}>{isLight ? "App Light" : "App Dark"} </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
-  );
+  ); 
 }
 
 export default App;
+
+// return (
+//   <div className={appClass}>
+//     <header>
+//       <h2>Shopster</h2>
+//       <button>Dark Mode</button>
+//     </header>
+//     <ShoppingList items={itemData} />
+//   </div>
+// );
+// }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [isInCart, setIsInCart] = useState(false) 
+
+  function addItem() {
+    setIsInCart(isInCart => !isInCart)
+  }
+  
+  const itemInCart = isInCart ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={itemInCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addItem}> {isInCart? "Remove From Cart" : "Add to Cart"} </button>
     </li>
   );
 }
